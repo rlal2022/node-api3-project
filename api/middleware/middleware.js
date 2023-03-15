1495c65d54e9db72b50b720bdd3327e1669f7d9a
rlal2022 <lrahul2010@gmail.com>
2023-03-15 01:43:24 -0700

    working on it

--- api/middleware/middleware.js
+++ api/middleware/middleware.js
@@ -1,9 +1,28 @@
+const Users = require("../users/users-model");
+
 function logger(req, res, next) {
   // DO YOUR MAGIC
+  // - `logger` logs to the console the following information about each request: request method, request url, and a timestamp
+  // - this middleware runs on every request made to the API
 }
 
 function validateUserId(req, res, next) {
   // DO YOUR MAGIC
+  // - this middleware will be used for all user endpoints that include an `id` parameter in the url (ex: `/api/users/:id` and it should check the database to make sure there is a user with that id.
+  // - if the `id` parameter is valid, store the user object as `req.user` and allow the request to continue
+  // - if the `id` parameter does not match any user id in the database, respond with status `404` and `{ message: "user not found" }`
+  Users.findbyId(req.params.id)
+    .then((user) => {
+      if (user) {
+        req.user = user;
+        next();
+      } else {
+        next({ status: 404, message: `user not found` });
+      }
+    })
+    .catch((err) => {
+      next(err);
+    });
 }
 
 function validateUser(req, res, next) {
--- api/server.js
+++ api/server.js
@@ -1,12 +1,14 @@
-const express = require('express');
-
+const express = require("express");
+const usersRouter = require("./users/users-router");
 const server = express();
 
 // remember express by default cannot parse JSON in request bodies
 
+server.use(express.json());
+// server.use("./api/users/users-router.js", usersRouter);
 // global middlewares and the user's router need to be connected here
 
-server.get('/', (req, res) => {
+server.get("/", (req, res) => {
   res.send(`<h2>Let's write some middleware!</h2>`);
 });
 
